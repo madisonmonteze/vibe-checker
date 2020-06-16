f468a665ac6ad01aa9422f0004d89f637844dfa7
Madison Monteze <madisonmonteze@gmail.com>
2020-06-16 09:00:11 -0400

    final styling

--- src/App.js
+++ src/App.js
@@ -13,12 +13,11 @@ class App extends Component {
           <h2>How's the vibe today?</h2>
           <Counter />
         </div>
-
-        <div className="footer">
-          <a href="#">About</a>
-        </div>
-      
         
+        <div className="footer">
+          <ToggleAbout />
+        </div>
+
       </div>
     );
   }
@@ -32,7 +31,7 @@ class Counter extends React.Component {
     };
   }
 
-increment() {
+  increment() {
     this.setState({
       count: this.state.count + 1
     });
@@ -61,9 +60,47 @@ increment() {
         <button className='reset' onClick={(e) => this.reset(e)}>Reset the vibes</button>
       </div>
 
-      <p>Current vibe count: {this.state.count}</p>
+      <p className="counter-box">Current vibe count: {this.state.count}</p>
   </div>
     );
+  }
+};
+
+class ToggleAbout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visibility: false
+    };
+    
+    this.toggleVisibility=this.toggleVisibility.bind(this);
+  }
+
+  toggleVisibility(){
+    this.setState({
+    visibility: !this.state.visibility
+  })
+  }
+  // change code above this line
+  render() {
+    if (this.state.visibility) {
+      return (
+        <div className="about-slider">
+          <button onClick={this.toggleVisibility}>Close</button>
+          <div className="about-content">
+            <p>This site was built with React.js and CSS by Madison Monteze. She can be found on <a href="https://twitter.com/madisonmonteze" alt="@madisonmonteze" target="_blank">Twitter</a>, <a href="https://github.com/madisonmonteze" alt="my github" target="_blank">Github</a>, and by email at <a href="mailto:madisonmonteze@gmail.com" alt="email me" target="_blank">madisonmonteze@gmail.com</a>.</p>
+            <p>Good vibes only!!!</p>
+          </div>
+          
+        </div>
+      );
+    } else {
+      return (
+        <div className="open-button">
+          <button onClick={this.toggleVisibility}>About</button>
+        </div>
+      );
+    }
   }
 };
 
